77156f73dba725d0582c0402e5aea4108a16f0b3
Sebastian Kürten <sebastian@topobyte.de>
2017-11-07 14:41:57 +0100

    Inital project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# What's this?
+
+This project is for testing language compatibility issues of JSweet.
---
+++ src/main/java/de/topobyte/SomeUtil.java
@@ -0,0 +1,16 @@
+package de.topobyte;
+
+public class SomeUtil
+{
+
+	public static void printSomething()
+	{
+		System.out.println("Hello JSweet");
+	}
+
+	public static String returnSomething()
+	{
+		return "Hello JSweet";
+	}
+
+}
